88dc581aa9436dd9ffae6caf9a3e3d57bd9504fc
Роман <rk-nk-sd@bk.ru>
2020-12-25 01:22:11 +0700

    Готово

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,13 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Needle7 {
+    @Autowired
+    private Deth8 deth;
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth.toString();
     }
 }
